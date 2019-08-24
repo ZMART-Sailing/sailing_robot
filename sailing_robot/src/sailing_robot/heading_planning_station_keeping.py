61193fc71c864af13443bb051bea3a7dd6522705
shenmishajing <shenmishajing@Gmail.com>
2019-08-24 11:09:27 +0800

    fix dbg_ball_position wrong name bug

--- sailing_robot/src/sailing_robot/heading_planning_station_keeping.py
+++ sailing_robot/src/sailing_robot/heading_planning_station_keeping.py
@@ -5,12 +5,12 @@ import json
 class HeadingPlan(heading_planning_laylines.HeadingPlan):
     def __init__(self, *args, **kwargs):
         super(HeadingPlan, self).__init__(*args, **kwargs)
-        self.debug_topics.append(('dbg_real_waypoint', 'String'))
+        self.debug_topics.append(('dbg_ball_position', 'String'))
 
     def calculate_state_and_goal(self):
         if len(self.nav.relative_position_list) > 0:
             self.nav.calculate_ball_position()
-            self.debug_pub('dbg_real_waypoint', json.dumps(
+            self.debug_pub('dbg_ball_position', json.dumps(
                 [self.nav.ball_position.lat.decimal_degree, self.nav.ball_position.lon.decimal_degree]))
             self.update_waypoint(self.nav.ball_position)
         return super(HeadingPlan, self).calculate_state_and_goal()
