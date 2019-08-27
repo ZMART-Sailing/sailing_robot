a2db0de84162664175ed55fd36a760c7a80ab804
shenmishajing <shenmishajing@Gmail.com>
2019-08-27 09:26:49 +0800

    add keep_station_points task

--- sailing_robot/src/sailing_robot/expand_task_dict.py
+++ sailing_robot/src/sailing_robot/expand_task_dict.py
@@ -73,6 +73,13 @@ def expand_task_dict(wp_params):
                     'radius': wp_task.get('radius', 3),
                     'accept_radius': wp_task.get('accept_radius', 15)
                 })
+            elif kind == 'keep_station_points':
+                expanded_task.update({
+                    'linger': wp_task.get('linger', 300),
+                    'radius': wp_task.get('radius', 3),
+                    'accept_radius': wp_task.get('accept_radius', 15),
+                    'threshold': wp_task.get('threshold', 10)
+                })
             elif kind == 'keep_station_obstacle':
                 expanded_task.update({
                     'linger': wp_task.get('linger', 180),
